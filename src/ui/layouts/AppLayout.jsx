60d67a58482b04e741407459fb4c6eb37246c392
kayler <pavelkayler@icloud.com>
2025-12-04 16:09:11 +0300

    Add footer user bar and widen quiz layout

--- src/ui/layouts/AppLayout.jsx
+++ src/ui/layouts/AppLayout.jsx
@@ -1,10 +1,12 @@
 import { Header } from "../components/header/Header.jsx";
+import { UserBar } from "../components/userBar/UserBar.jsx";
 
 const AppLayout = ({ children }) => {
   return (
     <div className="app-shell">
       <Header />
-      {children}
+      <main className="app-content">{children}</main>
+      <UserBar />
     </div>
   );
 };
